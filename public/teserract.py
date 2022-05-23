fd1ac8a634f7d0b28a17a98be9982265de51ec50
Ryeohwan <ahnrh951019@naver.com>
2022-05-23 16:13:57 +0900

    code: I add tesseract for scan words

---
+++ public/teserract.py
@@ -0,0 +1,6 @@
+import pytesseract
+from PIL import Image
+
+# pytesseract.pytesseract.tesseract_cmd = R'C:\Program Files\Tesseract-OCR\tesseract'
+str = pytesseract.image_to_string(Image.open('C:/Users/ahnrh/OneDrive/바탕 화면/graduate_project/public/image/paper.png'), lang='kor')
+print(str)
