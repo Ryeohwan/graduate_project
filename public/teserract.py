461cad1b852847cc39c5e0b23c14a2f4d1dbb58e
Ryeohwan <ahnrh951019@naver.com>
2022-05-24 23:59:41 +0900

    code: i

--- public/teserract.py
+++ public/teserract.py
@@ -4,3 +4,5 @@ from PIL import Image
 # pytesseract.pytesseract.tesseract_cmd = R'C:\Program Files\Tesseract-OCR\tesseract'
 str = pytesseract.image_to_string(Image.open('C:/Users/ahnrh/OneDrive/바탕 화면/graduate_project/public/image/paper.png'), lang='kor')
 print(str)
+
+# I have to
