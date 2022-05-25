d566020007dcdd23cbe6919d58b0b80ce3c3c93b
Ryeohwan <ahnrh951019@naver.com>
2022-05-25 23:59:51 +0900

    code;

--- public/teserract.py
+++ public/teserract.py
@@ -3,6 +3,13 @@ from PIL import Image
 
 # pytesseract.pytesseract.tesseract_cmd = R'C:\Program Files\Tesseract-OCR\tesseract'
 str = pytesseract.image_to_string(Image.open('C:/Users/ahnrh/OneDrive/바탕 화면/graduate_project/public/image/paper.png'), lang='kor')
-print(str)
 
-# I have to
+list_a = [i.replace(' ','') for i in str]
+i = 0
+for i in range(0,len(list_a)-1):
+    if list_a[i] == '':
+        list_a.remove('')
+    elif list_a[i] == '\n':
+        list_a.remove('\n')
+
+print(list_a)
