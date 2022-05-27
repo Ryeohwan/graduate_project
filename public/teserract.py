9e5315d4b972238b1ce4b80e46d3477ba8e5087e
Ryeohwan <ahnrh951019@naver.com>
2022-05-27 20:22:55 +0900

    code: I make light pretreatment code

--- public/teserract.py
+++ public/teserract.py
@@ -6,15 +6,40 @@ str = pytesseract.image_to_string(Image.open('C:/Users/ahnrh/OneDrive/바탕 화
 
 list_a = [i.replace(' ','') for i in str]
 list_b = []
-wrong_list = ['_','|','(',')','[',']','@','$','^','\\','/','※','*','′',',','%','~','>','<','、','-','=','.','…','”']
+wrong_text_list = ['_','|','(',')','[',']','@','$','^','\\','/','※','*','′',',','%','~','>','<','、','-','=','.','…','”','"']
 for i in range(len(list_a)):
     if list_a[i] != '':
-        if list_a[i] not in wrong_list:
+        if list_a[i] not in wrong_text_list:
             list_b.append(list_a[i])
 check = ''
 for i in range(len(list_b)):
     check += list_b[i]
 
 final_list = check.split()
-print(final_list)
+
+order_num = ''
+card_num = ''
+order_wrong = ['ㅜ문번호','수문번호','구무번호','주무번호','주우번호','주운번호']
+card_wrong = ['가드번호','카므번호','카드먼호','카드범호']
+for i in range(len(final_list)):
+    if '주문번호' in final_list[i]:
+        order_num = final_list[i]
+    elif '카드번호' in final_list[i]:
+        card_num = final_list[i]
+if order_num == '' and card_num == '':
+    for i in range(len(final_list)):
+        if final_list[i] in order_wrong:
+            order = final_list[i]
+        elif final_list[i] in card_wrong:
+            card_num = final_list[i]
+
+
+
+if order_num == '' and card_num == '':
+    print('reupload')
+else:
+    print(final_list)
+    print(f'{order_num}\n{card_num}')
+
+
 # success to extract words
