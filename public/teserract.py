74af09fc3d7939621af417fdff579f8813d2b0d2
Ryeohwan <ahnrh951019@naver.com>
2022-05-27 20:05:54 +0900

    code:I add exceptions

--- public/teserract.py
+++ public/teserract.py
@@ -2,11 +2,11 @@ import pytesseract
 from PIL import Image
 
 # pytesseract.pytesseract.tesseract_cmd = R'C:\Program Files\Tesseract-OCR\tesseract'
-str = pytesseract.image_to_string(Image.open('C:/Users/ahnrh/OneDrive/바탕 화면/graduate_project/public/image/paper.png'), lang='kor')
+str = pytesseract.image_to_string(Image.open('C:/Users/ahnrh/OneDrive/바탕 화면/graduate_project/public/image/paper.jpg'), lang='kor')
 
 list_a = [i.replace(' ','') for i in str]
 list_b = []
-wrong_list = ['_','|','(',')','[',']','@','$','^','\\','/']
+wrong_list = ['_','|','(',')','[',']','@','$','^','\\','/','※','*','′',',','%','~','>','<','、','-','=','.','…','”']
 for i in range(len(list_a)):
     if list_a[i] != '':
         if list_a[i] not in wrong_list:
