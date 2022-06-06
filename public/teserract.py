203f97cf95c6915e84d794fbd0bff8cc05e239e7
Ryeohwan <ahnrh951019@naver.com>
2022-06-06 23:51:16 +0900

    code: add wrong words

--- public/teserract.py
+++ public/teserract.py
@@ -20,7 +20,7 @@ final_list = check.split()
 order_num = ''
 card_num = ''
 
-for i in range(len(final_list)):  # find wrongs
+for i in range(len(final_list)):  # find wrong words
     if '주문번호' in final_list[i]:
         order_num = final_list[i]
     elif '카드번호' in final_list[i]:
